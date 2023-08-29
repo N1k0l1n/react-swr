973d401e2e3851dee7748c541845467d4940edff
Nikolin Andoni <97979235+N1k0l1n@users.noreply.github.com>
2023-08-29 09:56:28 +0200

    react-swr

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import useSWR, { SWRConfig } from "swr";
 
-function App() {
+const fetcher = (...args) => fetch(...args).then(res => res.json());
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SWRConfig value={{ revalidateOnFocus: false, fetcher }}>
+      <Products />
+    </SWRConfig>
   );
 }
 
-export default App;
+function Products() {
+  const url = "https://dummyjson.com/products";
+  const { data, error } = useSWR(url);
+
+  if (error) return <div>Error...</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return (
+    <DisplayProducts
+      products={data.products} // Accessing the nested products array
+      categories={[...new Set(data.products.map(product => product.category))]}
+    />
+  );
+}
+
+function DisplayProducts({ products, categories }) {
+  const [filterCategory, setFilterCategory] = React.useState(null);
+
+  const filteredProducts = filterCategory
+    ? products.filter(product => product.category === filterCategory)
+    : products;
+
+  return (
+    <>
+      {categories.map(category => (
+        <button
+          onClick={() => {
+            setFilterCategory(category);
+          }}
+          key={category}
+        >
+          {category}
+        </button>
+      ))}
+      {filterCategory && (
+        <button
+          onClick={() => {
+            setFilterCategory(null);
+          }}
+        >
+          reset
+        </button>
+      )}
+
+      <pre>{JSON.stringify(filteredProducts, null, 2)}</pre>
+    </>
+  );
+}
